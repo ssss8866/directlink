151811491a8bf3e192abaa6c6d618fac3362afce
Dainsleif <dainsleif@yeah.net>
2024-07-24 22:52:36 +0800

    2.1.0-beta

--- src/function/onedrive.js
+++ src/function/onedrive.js
@@ -1,44 +1,26 @@
-var data={};
-
 function getApi(Key) {
 	const rule0=/:/;
 	const rule1=/\?(.*):/;
 	const rule2=/:(.*)/;
 	const rule3=/\?(.*)/;
-	let Pwd="";
+	let ShareKey,Pwd;
 	if(rule0.test(Key)) {
-		data.ShareKey = Key.match(rule1)[1];
+		ShareKey = Key.match(rule1)[1];
 		Pwd = Key.match(rule2)[1];
-	}else data.ShareKey = Key.match(rule3)[1];
-	return "https://www.123pan.com/b/api/share/get?limit=100&next=1&orderBy=share_id&orderDirection=desc&ParentFileId=0&Page=1&shareKey="+data.ShareKey+"&Sharepwd="+Pwd;
+	}else ShareKey = Key.match(rule3)[1];
+	return 'https://www.123pan.com/b/api/share/get?limit=100&next=1&orderBy=share_id&orderDirection=desc&ParentFileId=0&Page=1&shareKey='+data.ShareKey+'&Sharepwd='+Pwd;
 }
 
 async function getData(Key) {
 	const api = getApi(Key);
 	let info=await fetch(api);
 	info=await info.json();
+	let data = {};
 	data.FileId=info.data.InfoList[0].FileId;
 	data.S3KeyFlag=info.data.InfoList[0].S3KeyFlag;
 	data.Size=info.data.InfoList[0].Size;
 	data.Etag=info.data.InfoList[0].Etag;
 	return JSON.stringify(data);
-}
-
-function crc32(str) {
-	let crc = 0xFFFFFFFF;
-
-	for (let i = 0; i < str.length; i++) {
-		crc ^= str[i];
-		for (let j = 0; j < 8; j++) {
-			if ((crc & 1) !== 0) {
-				crc = (crc >>> 1) ^ 0xEDB88320;
-			} else {
-				crc >>>= 1;
-			}
-		}
-	}
-	crc ^= 0xFFFFFFFF;
-	return crc >>> 0;
 }
 
 async function getSign() {
@@ -47,23 +29,21 @@ async function getSign() {
 	timestamp=JSON.stringify(timestamp.data.timestamp);
 	const table = ['a', 'd', 'e', 'f', 'g', 'h', 'l', 'm', 'y', 'i', 'j', 'n', 'o', 'p', 'k', 'q', 'r', 's', 't', 'u', 'b', 'c', 'v', 'w', 's', 'z'];
 	const random = Math.floor(1e7 * Math.random()).toString();
-	const path = "/b/api/share/download/info";
-	const os = "web";
+	const path = '/b/api/share/download/info';
+	const os = 'web';
 	const version = 3;
-	const now = new Date(timestamp * 1000);
 	const nowStr = now.toISOString().replace(/-/g, '').replace(/:/g, '').replace(/\..*$/, '');
-	let charStr = "";
 	for (let i = 0; i < nowStr.length; i++) {
-		charStr = charStr + table[nowStr[i].charCodeAt(0) - 48];
+		nowStr[i] = table[nowStr[i].charCodeAt(0) - 48];
 	}
-	const timeSign = crc32(charStr).toString();
+	const timeSign = crc32.crc32IEEE(nowStr).toString();
 	const data = [timestamp, random, path, os, version, timeSign].join('|');
-	const dataSign = crc32(data).toString();
+	const dataSign = crc32.crc32IEEE(data).toString();
 	return [timeSign, [timestamp, random, dataSign].join('-')].join('=');
 }
 
 async function request(token,data,sign) {
-	const aim="https://www.123pan.com/b/api/share/download/info?"+sign;
+	const aim='https://www.123pan.com/b/api/share/download/info?'+sign;
 	const init={
 		method:"POST",
 		headers:{
@@ -71,7 +51,7 @@ async function request(token,data,sign) {
 			"platform":"web",
 			"Authorization":token,
 		},
-		body:await data,
+		body:data,
 	};
 	let res=await fetch(aim,init);
 	res=JSON.stringify(await res.json());
@@ -81,7 +61,7 @@ async function request(token,data,sign) {
 	return atob(res).match(rule1)[1];
 }
 
-export async function getUrl(token,Key) {
+export function getUrl(token,Key) {
 	const data = getData(Key);
 	const sign = getSign();
 	return request(token,data,sign);
